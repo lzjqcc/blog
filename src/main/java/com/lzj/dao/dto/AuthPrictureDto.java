cb0206a352f1f6aab1a406b6257e48de2c362d20
lzj <1161889163@qq.com>
2018-05-11 11:36:00 +0800

    a

---
+++ src/main/java/com/lzj/dao/dto/AuthPrictureDto.java
@@ -0,0 +1,10 @@
+package com.lzj.dao.dto;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+
+public class AuthDto implements Serializable {
+    private static final long serialVersionUID = -1730649115329465454L;
+    private Integer id;
+    private String friendName;
+}
---
+++ src/main/java/com/lzj/dao/dto/PictureDto.java
@@ -0,0 +1,4 @@
+package com.lzj.dao.dto;
+
+public class PictureDto {
+}
